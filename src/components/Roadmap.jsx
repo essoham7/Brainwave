6796637922886e2fdccd9daa931ff3f1562fd46b
Essoham <richessoham@gmail.com>
2024-05-05 17:21:51 +0000

    feat(UI): added footer fixed roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,10 +1,11 @@
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./TagLine";
+
 import { roadmap } from "../constants";
-import { check2, grid, loading1 } from "../../public/assets";
+import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
+import TagLine from "./Tagline";
 
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
@@ -34,7 +35,7 @@ const Roadmap = () => (
                 </div>
                 <div className="relative z-1">
                   <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
-                    <Tagline>{item.date}</Tagline>
+                    <TagLine>{item.date}</TagLine>
 
                     <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
                       <img
@@ -69,7 +70,7 @@ const Roadmap = () => (
       </div>
 
       <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Our roadmap</Button>
+        <Button href="/#roadmap">Our roadmap</Button>
       </div>
     </div>
   </Section>
